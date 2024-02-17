d882296bcd9f11350bcf2e770803714c5e42e919
Ismail Bennani <lahkim.bennani.ismail@gmail.com>
2024-02-17 14:21:05 +0100

    bump version

--- Lib/Metadata.cs
+++ Lib/Metadata.cs
@@ -2,7 +2,7 @@ namespace PalworldDataExtractor;
 
 public static class Metadata
 {
-    public const string Version = "0.1.0";
+    public const string Version = "0.1.1";
     public const string Author = "Ismail Bennani";
     public const string Repository = "https://github.com/PalworldDataTools/PalworldDataExtractor";
 }
