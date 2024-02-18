1fdd89028ae3fc523b695fba7139abc0d1414320
Ismail Bennani <lahkim.bennani.ismail@gmail.com>
2024-02-18 19:22:09 +0100

    abstract serialization/deserialization of extracted data

--- Lib/Models/ExtractedData.cs
+++ Lib/Models/ExtractedData.cs
@@ -1,13 +1,19 @@
-﻿using PalworldDataExtractor.Models.Pals;
+﻿using System.Text.Json;
+using PalworldDataExtractor.Models.Pals;
 using PalworldDataExtractor.Models.Steam;
 
 namespace PalworldDataExtractor.Models;
 
 public class ExtractedData
 {
+    static readonly JsonSerializerOptions SerializerOptions = new() { WriteIndented = true };
+
     public required SteamManifest? SteamManifest { get; init; }
     public required IReadOnlyCollection<PalTribe> Tribes { get; init; }
     public required IReadOnlyDictionary<string, byte[]> TribeIcons { get; init; }
 
     internal ExtractedData() { }
+
+    public static async Task Serialize(ExtractedData data, Stream outStream) => await JsonSerializer.SerializeAsync(outStream, data, SerializerOptions);
+    public static async Task<ExtractedData?> Deserialize(Stream stream) => await JsonSerializer.DeserializeAsync<ExtractedData>(stream, SerializerOptions);
 }
